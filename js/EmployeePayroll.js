d0471ba1308db235cc7c97ce46e83b1600c413bc
Darpan Madlani <Darpan@proclink.com>
2022-01-27 20:49:49 +0530

    validating form

---
+++ js/EmployeePayroll.js
@@ -0,0 +1,42 @@
+class EmployeePayroll {
+
+    get id() { return this._id };
+    set id(id) {
+        this._id = id;
+    }
+
+    get name() { return this._name; }
+    set name(name) {
+        this._name = name;
+    }
+
+    get profilePic() { return this._profilePic; }
+    set profilePic(profilePic) {
+        this._profilePic = profilePic;
+    }
+
+    get gender() { return this._gender; }
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() { return this._department; }
+    set department(department) {
+        this._department = department;
+    }
+
+    get salary() { return this._salary; }
+    set salary(salary) {
+        this._salary = salary;
+    }
+
+    get notes() { return this._notes; }
+    set notes(notes) {
+        this._notes = notes;
+    }
+
+    get startDate() { return this._startDate; }
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+}
--- js/EmployeePayrollForm.js
+++ js/EmployeePayrollForm.js
@@ -1,7 +1,48 @@
-function getSalary(value) {
-    document.getElementById("#output1").innerHTML(value);
-}
+var salary = document.getElementById("salary");
+var output = document.getElementById("rangeValue");
+
+salary.oninput = function() {
+    output.innerHTML = salary.value;
+};
+salary.onloadeddata = function() {
+    output.innerHTML = salary.value;
+};
 
 function save() {
-    alert("Form submitted");
+    var name = document.getElementById("empForm").elements.namedItem("name").value;
+    var profile = document.getElementById("empForm").elements.namedItem("profile").value;
+    var gender = document.getElementById("empForm").elements.namedItem("gender").value;
+    var hrSelected = document.getElementById("empForm").elements.namedItem("hr").checked;
+    var salary = document.getElementById("empForm").elements.namedItem("salary").value;
+    var day = document.getElementById("empForm").elements.namedItem("Day").value;
+    var month = document.getElementById("empForm").elements.namedItem("Month").value;
+    var year = document.getElementById("empForm").elements.namedItem("Year").value;
+
+    let nameRegex = RegExp('[A-Z]{1}[a-zA-Z]{2,}$');
+
+    let flag = true;
+    if (!nameRegex.test(name)) {
+        document.getElementById("nameError").innerHTML = "Name is incorrect";
+        flag = false;
+    } else {
+        document.getElementById("nameError").innerHTML = "";
+    }
+
+    if (profile == '') {
+        document.getElementById("profileError").innerHTML = "Profile must be selected";
+        flag = false;
+    } else {
+        document.getElementById("profileError").innerHTML = "";
+    }
+    var startDate = day + "/" + month + "/" + year
+    const emp = {
+        name: name,
+        profile: profile,
+        gender: gender,
+        hrSelected: hrSelected,
+        salary: salary,
+        startDate: startDate
+    }
+    alert(emp.name + "\n" + emp.profile + "\n" + emp.gender + "\n" + emp.hrSelected + "\n" + emp.salary + "\n" + emp.startDate);
+    return flag;
 }
